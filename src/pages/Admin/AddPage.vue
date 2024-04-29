728cfebd7904860768d76f0f839170e297e3fd28
Razan Muhammad <razanmfs507@gmail.com>
2024-04-29 14:09:55 +0700

    dklsajdlsa

--- src/pages/Admin/AddPage.vue
+++ src/pages/Admin/AddPage.vue
@@ -2,12 +2,12 @@
   <div class="text-center text-h6 text-bold q-mt-md">Edit Section Hero</div>
   <div class="text-center q-mt-sm">Ubah dan alur konten web Keraton</div>
 
-  <div style="padding-inline: 300px">
+  <div style="padding-inline: 300px; margin-top: 120px">
     <div class="flex full-width" style="gap: 5px">
       <div class="col-grow">
         <q-input
           filled
-          v-model="text"
+          v-model="title"
           label="Title"
           color="black"
           bg-color="gray"
@@ -15,41 +15,59 @@
       </div>
       <q-input
         filled
-        v-model="text"
+        v-model="sizeTitle"
         label="Size"
         color="black"
         bg-color="gray"
       />
     </div>
 
-    <div class="flex full-width" style="gap: 5px">
+    <div class="flex full-width q-mt-md" style="gap: 5px">
       <div class="col-grow">
         <q-input
           filled
-          v-model="text"
-          label="Title"
+          v-model="subTitle"
+          label="Sub Title"
           color="black"
           bg-color="gray"
         />
       </div>
       <q-input
         filled
-        v-model="text"
+        v-model="subTitleSize"
         label="Size"
         color="black"
         bg-color="gray"
       />
     </div>
-    <q-file filled v-model="model" label="Filled" color="black" />
+    <q-file
+      filled
+      v-model="uploadFile"
+      label="Tambahkan File"
+      color="black"
+      class="q-mt-md"
+    />
     <q-btn
-      color="white"
-      text-color="black"
-      label="Standard"
-      class="full-width"
+      no-caps
+      style="background: #123b32"
+      text-color="white"
+      label="Save and Update"
+      class="full-width q-mt-md"
     />
   </div>
 </template>
 
 <script>
-export default {};
+import ref from "vue";
+export default {
+  setup() {
+    return {
+      title: ref(),
+      sizeTitle: ref(),
+      subTitle: ref(),
+      subTitleSize: ref(),
+      uploadFile: ref(),
+    };
+  },
+};
 </script>
